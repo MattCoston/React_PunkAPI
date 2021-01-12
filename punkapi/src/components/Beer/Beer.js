2bfcab019ff0cbe19afd0eb943da4c79b2ded588
Matt Coston <mattcoston7@gmail.com>
2021-01-11 18:49:33 -0600

    started styling beer api cards

--- punkapi/src/components/Beer/Beer.js
+++ punkapi/src/components/Beer/Beer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Beer.css';
 
 class Beer extends Component {
     constructor(props) {
@@ -8,9 +9,10 @@ class Beer extends Component {
         return (
             <div>
                 <h1>{this.props.name}</h1>
-                <h3>{this.props.tagline}</h3>
-                <h6>{this.props.brewers_tips}</h6>
-                <img src={this.props.image} />
+                <img src={this.props.image} className="beer__image" />
+                <br></br>
+                <span className="beer__tagline">{this.props.tagline}</span>
+                <h6>{this.props.tips}</h6>
             </div>
         );
     }
